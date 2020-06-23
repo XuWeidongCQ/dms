eba3f8f95b04bf55484208e54935836366a32ae0
XuWeidongCQ <1455049258@qq.com>
2020-06-23 19:29:12 +0800

    完成仪器卡片评价情况饼图

--- src/components/share-components/xDevInfoCard.vue
+++ src/components/share-components/xDevInfoCard.vue
@@ -1,34 +1,79 @@
 
 <template>
   <x-box class="dev-card-wrapper">
-    <!-- 仪器图片 -->
-    <div class="dev-img-wrapper">
-      <img :src="imgUrl">
-    </div>
-    <!-- 仪器说明 -->
-    <div class="dev-describe-wrapper">
-      <p>厂商：{{ devCompany }}</p>
-      <p>产品：{{ devName }}</p>
-      <p>型号：{{ devType }}</p>  
-    </div>
-    <div class="dev-statistic-wrapper">
-      <div class="left">
-        <span>仪器序列号</span>
-        <ul class="dev-serial-number-wrapper xu-add-scrollBar">
-          <li 
-          v-for="(item,index) in serialNumberList" 
-          :key="index"
-          :class="{'active':item === selSerialNumber}"
-          @click="changeSerialNumber(item)"
-          >
-            {{ item }}
-          </li>
-        </ul>
+    <!-- 第一行 -->
+    <div class="dev-evaluation-wrapper">
+      <!-- 仪器信息 -->
+      <div class="dev-info-wrapper">
+        <div class="dev-img-wrapper">
+          <img :src="imgUrl">
+        </div>
+        <div class="dev-describe-wrapper">
+          <p>厂商：{{ devCompany }}</p>
+          <p>产品：{{ devName }}</p>
+          <p>型号：{{ devType }}</p>  
+        </div>
       </div>
-      <div class="right">
+      <div class="experience-pie-wrapper">
         <div class="chart-wrapper">
+          <x-pie-chart
+          titleText="使用体验"
+          :radius="['45%','60%']"
+          :center="['50%','37%']"
+          v-if="devEvaluationInfo"
+          :source="{'itemName':Object.keys(this.devEvaluationInfo.experienceLevel),
+                    'itemValue':Object.values(this.devEvaluationInfo.experienceLevel)}"
+          :legend="initLegend()"
+          > 
+          </x-pie-chart>
+        </div> 
+      </div>
+      <div class="reliable-pie-wrapper">
+        <div class="chart-wrapper">
+          <x-pie-chart
+          titleText="临床可靠性"
+          :radius="['45%','60%']"
+          :center="['50%','37%']"
+          v-if="devEvaluationInfo"
+          :source="{'itemName':Object.keys(this.devEvaluationInfo.reliabilityLevel),
+                    'itemValue':Object.values(this.devEvaluationInfo.reliabilityLevel)}"
+          :legend="initLegend()"
+          > 
+          </x-pie-chart>
+        </div> 
+      </div>
+      <div class="error-pie-wrapper">
+        <div class="chart-wrapper">
+          <x-pie-chart
+          titleText="术中故障情况"
+          :color="['#4ea397','#d0648a']"
+          :radius="['45%','60%']"
+          :center="['50%','37%']"
+          v-if="devEvaluationInfo"
+          :source="{'itemName':['无故障','有故障'],
+                    'itemValue':Object.values(this.devEvaluationInfo.error)}"
+          :legend="initLegend()"
+          > 
+          </x-pie-chart>
+        </div>
+      </div>
+    </div>
+    <!-- 第二行 -->
+    <div class="spec-dev-evaluation-wrapper bg-gray">
+      <div class="serial-wrapper xu-add-scrollBar">
+        <x-list 
+        :items="serialNumberList"
+        :activeItem="selSerialNumber"
+        @click="changeSerialNumber($event)"
+        >
+        </x-list>
+      </div>
+      <div class="spec-dropout-pie-wrapper">
+        <div class="dropout-chart-wrapper">
           <x-inner-pie-chart 
           :titleText="'平均掉线率'"
+          :radius="['55%','70%']"
+          :center="['50%','55%']"
           :color="['#019b4c','#e62229','#e0a800']"
           :source="{'y':[1-statisticInfo.averageDropRate,statisticInfo.averageDropRate]}"
           :isModalChart="true"
@@ -41,7 +86,47 @@
         :value="'已完成' + opeNum + '场手术'"
         :type="'success'"
         @click="emitSelSerialNumber(true)"
-        ></x-button>
+        ></x-button> 
+      </div>
+      <div class="spec-experience-pie-wrapper">
+        <div class="chart-wrapper">
+          <x-pie-chart
+          titleText="使用体验"
+          :center="['50%','37%']"
+          v-if="specDevEvaluationInfo"
+          :source="{'itemName':Object.keys(this.specDevEvaluationInfo.experienceLevel),
+                    'itemValue':Object.values(this.specDevEvaluationInfo.experienceLevel)}"
+          :legend="initLegend()"
+          > 
+          </x-pie-chart>
+        </div> 
+      </div>
+      <div class="spec-reliable-pie-wrapper">
+        <div class="chart-wrapper">
+          <x-pie-chart
+          titleText="临床可靠性"
+          :center="['50%','37%']"
+          v-if="specDevEvaluationInfo"
+          :source="{'itemName':Object.keys(this.specDevEvaluationInfo.reliabilityLevel),
+                    'itemValue':Object.values(this.specDevEvaluationInfo.reliabilityLevel)}"
+          :legend="initLegend()"
+          > 
+          </x-pie-chart>
+        </div> 
+      </div>
+      <div class="spec-error-pie-wrapper">
+        <div class="chart-wrapper">
+          <x-pie-chart
+          titleText="术中故障情况"
+          :color="['#4ea397','#d0648a']"
+          :center="['50%','37%']"
+          v-if="specDevEvaluationInfo"
+          :source="{'itemName':['无故障','有故障'],
+                    'itemValue':Object.values(this.specDevEvaluationInfo.error)}"
+          :legend="initLegend()"
+          >
+          </x-pie-chart>
+        </div> 
       </div>
     </div>
   </x-box>
@@ -51,11 +136,15 @@
 import xBox from '@/x-views/xBox'
 import xInnerPieChart  from '@/components/share-components/xInnerPieChart'
 import xButton from '@/x-views/xButton'
+import xList from '@/x-views/xList'
+import xPieChart from '@/components/share-components/xPieChart'
 export default {
   components: {
     xBox,
     xInnerPieChart,
-    xButton
+    xButton,
+    xList,
+    xPieChart
   },
   props:{
     //1.接受一个deviceCode
@@ -93,7 +182,9 @@ export default {
     return {
       serialNumberList:[],
       statisticInfo:{},
-      selSerialNumber:'0'
+      selSerialNumber:'0',
+      devEvaluationInfo:null,
+      specDevEvaluationInfo:null
     }
   },
   computed:{
@@ -106,6 +197,19 @@ export default {
     }
   },
   methods:{
+    //0.初始化饼图的图例
+    initLegend(){
+      return {
+        show:true,
+        orient:'vertical',
+        top:'155',
+        itemGap:2,
+        itemHeight:12,
+        textStyle:{
+          fontSize:12
+        }
+      }
+    },
     //1.获取所有的序列号
     getSerialNumber(deviceCode){
       this.$http['getDevCardSerialNumbers']({
@@ -132,9 +236,29 @@ export default {
     },
     //3.改变仪器序列号
     changeSerialNumber(deviceSerialNumber){
+      // console.log(deviceSerialNumber)
       this.selSerialNumber = deviceSerialNumber
     },
-    //4.点击按钮
+    //4.获取这个型号仪器的评价统计信息
+    getDevEvaluationInfo(deviceCode){
+      this.$http['getDevEvaluationInfos']({
+        params:{deviceCode:deviceCode}
+      }).then(res => {
+        const {data} = res
+        data ? this.devEvaluationInfo = data:'';
+      })
+    },
+    //5.获取某一个特定序列号仪器的评价统计信息
+    getSpecDevEvaluationInfo(deviceCode,deviceSerialNumber){
+      this.$http['getSpecDevEvaluationInfos']({
+        params:{deviceCode:deviceCode,serialNumber:deviceSerialNumber}
+      }).then(res => {
+        const {data} = res
+        data ? this.specDevEvaluationInfo = data:'';
+        // console.log(this.specDevEvaluationInfo.experienceLevel)
+      })
+    },
+    //6.点击按钮
     emitSelSerialNumber(isClick){
       this.$emit(
         'getOpeInfoOfThisDev',
@@ -150,11 +274,13 @@ export default {
   watch:{
     'selSerialNumber':function(newVal){
       this.getStatisticInfo(this.deviceCode,newVal)
+      this.getSpecDevEvaluationInfo(this.deviceCode,newVal)
       // this.emitSelSerialNumber()
     }
   },
   created(){
     this.getSerialNumber(this.deviceCode)
+    this.getDevEvaluationInfo(this.deviceCode)
   }
 }
 </script>
@@ -162,63 +288,71 @@ export default {
 <style scoped>
 .dev-card-wrapper {
   width: 775px;
-  /* display: inline-block; */
   margin:0 15px 15px 15px;
+}
+.dev-evaluation-wrapper {
+  display: flex;
+  margin-bottom: 10px;
+}
+.dev-info-wrapper{
+  flex:1.2
+}
+.experience-pie-wrapper,
+.reliable-pie-wrapper,
+.error-pie-wrapper{
+  flex:1;
+  text-align: center;
+  box-sizing: border-box;
+}
+.dev-info-wrapper .dev-describe-wrapper>p {
+  font-size: 12px;
+  white-space: nowrap;
+  overflow: hidden;
 }
 
 .dev-img-wrapper {
-  margin-bottom: 10px;
-}
-.dev-img-wrapper > img {
-  height: 200px;
-}
-.dev-describe-wrapper > p{
-  font-size: 12px;
-}
-.dev-statistic-wrapper {
-  position: absolute;
-  background-color: rgba(245, 245, 245);
-  width: 260px;
-  top: 20px;
-  right: 20px;
-  bottom: 20px;
-  display: flex;
-  padding: 5px 0;
-  border-radius: 5px;
-  font-size: 12px;
-}
-.dev-statistic-wrapper > div:first-child {
-  flex: 2;
-  margin-right: 10px;
-}
-.dev-statistic-wrapper > div:last-child {
-  flex: 3;
+  margin-bottom: 5px;
   text-align: center;
 }
-.dev-statistic-wrapper  span {
-  display: inline-block;
-  font-size: 14px;
-  padding-bottom: 5px;
+.dev-img-wrapper > img {
+  height: 180px;
+}
+
+.spec-dev-evaluation-wrapper {
+  display: flex;
+  border-right: 1px solid #24c79f;
+  font-size: 12px;
+}
+.serial-wrapper{
+  flex: 0.8;
   box-sizing: border-box;
-  color: #111;
 }
-.dev-serial-number-wrapper > li{
-  white-space: nowrap;
-  overflow: hidden;
-  padding: 5px 0 5px 5px;
-  cursor: pointer;
+.spec-dropout-pie-wrapper{
+  border-left: 1px solid #24c79f;
 }
-.right > p {
-  margin-bottom: 10px;
+.spec-dropout-pie-wrapper,
+.spec-experience-pie-wrapper,
+.spec-reliable-pie-wrapper,
+.spec-error-pie-wrapper{
+  flex:1;
+  box-sizing: border-box;
+  padding: 5px 0;
 }
-
+.spec-dropout-pie-wrapper,
+.spec-experience-pie-wrapper,
+.spec-reliable-pie-wrapper,
+.spec-error-pie-wrapper{
+  border-top:1px solid #24c79f;
+  border-bottom:1px solid #24c79f;
+  text-align: center;
+}
+.pie-title {
+  font-size: 14px;
+}
+.dropout-chart-wrapper {
+  height: 160px;
+}
 .chart-wrapper {
-  height: 150px;
-}
-
-.active {
-  color: #ffffff;
-  background-color: #24c79f;
-  transition: background-color 0.4s;
+  height: 237px;
 }
 </style>
---
+++ src/x-views/xList.vue
@@ -0,0 +1,52 @@
+<template>
+  <ul class="list-wrapper">
+    <li
+    class="list-item-wrapper"
+    :class="{'active':item === activeItem}" 
+    v-for="(item,index) in items" 
+    :key="index" 
+    @click="emit(item)">
+      {{ item }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props:{
+    items:{
+      type:Array,
+      default:() => []
+    },
+    activeItem:{
+      default:''
+    }
+  },
+  methods:{
+    emit(value){
+      this.$emit('click',value)
+    }
+  }
+}
+</script>
+
+<style scoped>
+ul {
+  margin: 0;
+  padding: 0;
+}
+li {
+  list-style: none;
+}
+.list-wrapper{
+  font-size: 14px;
+}
+.list-wrapper > .list-item-wrapper {
+  cursor: pointer;
+  padding: 5px 2px;
+}
+.active {
+  background-color: #24c79f !important;
+  color: #ffffff !important;
+}
+</style>
